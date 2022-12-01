c0856b07c33e2c2708a0717e5aa426a795f8a570
grantgariepy <grantgariepy@protonmail.com>
2022-11-30 21:50:30 -0700

    search working!

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,16 @@
+'use client';
+
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import food from '../../public/assets/food.jpeg';
+import Link from 'next/link';
 
 export default function Hero() {
+  const [term, setTerm] = useState('');
+  // console.log(term);
+  const handleParam = (setValue: any) => (e: any) => setValue(e.target.value);
+
   return (
     <div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
       <div className='max-w-screen-2xl px-4 md:px-8 mx-auto'>
@@ -15,14 +24,26 @@ export default function Hero() {
               So many recipes to choose from! You can search by catergory, ingredients, or
               geographical area! Or if you're feeling lucky you can get a random recipe!
             </p>
-            <form className='w-full md:max-w-md flex gap-2'>
+            <form
+              className='w-full md:max-w-md flex gap-2'
+              // onSubmit={handleSubmit}
+              action={`results/${term}`}
+            >
               <input
-                placeholder='Search'
+                type='text'
+                id='name'
+                // name='q'
+                placeholder='Search by Ingredients'
                 className='w-full flex-1 bg-gray-50 text-gray-800 placeholder-gray-500 border focus:ring ring-green-300 rounded outline-none transition duration-100 px-3 py-2'
+                onChange={handleParam(setTerm)}
+                value={term}
               />
-              <button className='inline-block bg-green-500 hover:bg-green-600 active:bg-green-700 focus-visible:ring ring-green-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2'>
+              <Link
+                href={`results/${term}`}
+                className='inline-block bg-green-500 hover:bg-green-600 active:bg-green-700 focus-visible:ring ring-green-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2'
+              >
                 Search
-              </button>
+              </Link>
             </form>
           </div>
           {/* content - end */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import CallToAction from './components/CallToAction';
 import Hero from './components/Hero';
+import RandomSelection from './components/RandomSelection';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      {/* @ts-ignore */}
+      <RandomSelection />
+      <CallToAction />
     </>
   );
 }
